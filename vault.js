4731f70e49d9a689f3a72d2e7376cbc9a889e998
Andy <suhandyl@gmail.com>
2016-06-09 14:22:17 -1000

    Finished first 2 tests

--- vault.js
+++ vault.js
@@ -1,4 +1,19 @@
 'use strict';
 module.exports = function() {
-  
+
+  var vault = {
+
+    setValue: function (key, value) {
+      vault.key = value;
+    },
+
+    getValue: function (key){
+      if (vault.hasOwnProperty('key')){
+        return value;
+      } else {
+        return null;
+      }
+    }
+  };
+  return vault;
 };
